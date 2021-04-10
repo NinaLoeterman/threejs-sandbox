ab4e7bbeaeeb6cc08f555f973e7956fdf888974d
Nina Loeterman <nina.loeterman@gmail.com>
2021-04-10 15:21:28 +0300

    texture and suspense added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { OrbitControls, Sky, Stars, Sphere } from "@react-three/drei";
+import { TextureLoader } from "three";
+import dog from "./assets/dog.jpg";
 import "./App.css";
-import { OrbitControls, Stars } from "@react-three/drei";
 
 function Box() {
   const mesh = useRef();
+  const texture = useLoader(TextureLoader, dog);
 
   useFrame(() => {
     mesh.current.rotation.x += 0.02;
@@ -13,20 +16,22 @@ function Box() {
 
   return (
     <mesh ref={mesh}>
-      <meshStandardMaterial attach="material" />
-      <boxBufferGeometry attach="geometry" color="#fa1852" />
+      <meshStandardMaterial map={texture} attach="material" color="skyblue" />
+      <boxBufferGeometry attach="geometry" />
     </mesh>
   );
 }
 
 function App() {
   return (
-    <Canvas style={{height: "100vh"}}>
+    <Canvas>
       <OrbitControls />
       <Stars />
       <ambientLight intensity={0.2} />
-      <directionalLight position={[10, 0, 0]}/>
-      <Box />
+      <directionalLight position={[10, 0, 0]} />
+      <Suspense fallback="...loading">
+        <Box />
+      </Suspense>
     </Canvas>
   );
 }
